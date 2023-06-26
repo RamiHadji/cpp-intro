dc73a39c260c52be050f82654e286c3871cfc3b4
RamiHadji <43167480+RamiHadji@users.noreply.github.com>
2023-06-26 13:49:04 +0100

    Add files via upload

---
+++ Homework_3 Lesson #18/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int Age;
+    cout << "Please enter your age: ";
+    cin >> Age;
+
+    int YourAgeAfter5Years = Age + 5;
+
+    cout << "After 5 years, you will be " << YourAgeAfter5Years << " years old." << endl;
+
+    return 0;
+}
